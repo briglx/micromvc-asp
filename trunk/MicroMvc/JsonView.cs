7eb0a1240c714889e6af13278c40c749e2e3a38c
LGarcia69 <LGarcia69@802736f6-a1f5-11dd-8ab5-df3da3b84efe>
2008-11-20 17:56:45 +0000

    Added for Json formatting

---
+++ trunk/MicroMvc/JsonView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Web;
+
+
+namespace MicroMvc
+{
+    public class JsonView<T> : IBaseView<T>
+    {
+
+        public T ViewData { get; set; }
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+
+            // Perform JSON to datawrapper collection returning string value
+            string result = JSONSerializer.ToJSON(this.ViewData);
+           //result = jsonCB + "(" + result + ")";
+
+            //XmlSerializer x = new XmlSerializer(this.ViewData.GetType());
+            //StringWriter stream = new StringWriter();
+            //x.Serialize(stream, this.ViewData);
+            //stream.Flush();
+            //String result = stream.ToString();
+
+            context.Response.ContentType = "text/json";
+            context.Response.Write(result);
+            context.Response.End();
+        }
+    }
+}
