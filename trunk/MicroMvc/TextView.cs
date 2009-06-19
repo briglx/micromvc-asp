e6de52eee87b95a0c17d7ed388299e6fe4619f3f
blamoreaux <blamoreaux@802736f6-a1f5-11dd-8ab5-df3da3b84efe>
2009-06-19 22:18:40 +0000

    Added ContentType to TextView

--- trunk/MicroMvc/TextView.cs
+++ trunk/MicroMvc/TextView.cs
@@ -8,6 +8,13 @@ namespace MicroMvc
     public class TextView<T> : IBaseView<T>
     {
         public T ViewData { get; set; }
+        public string ContentType { get; set; }
+
+        public TextView()
+        {
+            this.ContentType = "text/text";
+        }
+
         public bool IsReusable
         {
             get { return false; }
@@ -17,7 +24,7 @@ namespace MicroMvc
         {
             string result = this.ViewData.ToString();
 
-            context.Response.ContentType = "text/text";
+            context.Response.ContentType = this.ContentType;
             context.Response.Write(result);
             context.Response.End();
         }
